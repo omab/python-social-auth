b9c75eb9d2e17a233bd182a0bed9f9bceafd8bde
Matías Aguirre <matiasaguirre@gmail.com>
2012-12-16 22:51:01 -0200

    LiveJournal backend

---
+++ social/backends/livejournal.py
@@ -0,0 +1,34 @@
+"""
+LiveJournal OpenID support.
+
+This contribution adds support for LiveJournal OpenID service in the form
+username.livejournal.com. Username is retrieved from the identity url.
+"""
+import urlparse
+
+from social.backends.open_id import OpenIdAuth
+from social.exceptions import AuthMissingParameter
+
+
+class LiveJournalOpenId(OpenIdAuth):
+    """LiveJournal OpenID authentication backend"""
+    name = 'livejournal'
+
+    def get_user_details(self, response):
+        """Generate username from identity url"""
+        values = super(LiveJournalOpenId, self).get_user_details(response)
+        values['username'] = values.get('username') or \
+                             urlparse.urlsplit(response.identity_url)\
+                                        .netloc.split('.', 1)[0]
+        return values
+
+    def openid_url(self):
+        """Returns LiveJournal authentication URL"""
+        if not self.data.get('openid_lj_user'):
+            raise AuthMissingParameter(self, 'openid_lj_user')
+        return 'http://%s.livejournal.com' % self.data['openid_lj_user']
+
+
+BACKENDS = {
+    'livejournal': LiveJournalOpenId,
+}
