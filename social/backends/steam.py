f1fdeef484c48199fd1a97d4eefb6d1208f20dd0
Matías Aguirre <matiasaguirre@gmail.com>
2013-02-17 13:49:52 -0200

    Steam backend

---
+++ social/backends/steam.py
@@ -0,0 +1,56 @@
+"""Steam OpenId support"""
+import re
+import json
+import urllib
+
+from social.backends.open_id import OpenIdAuth
+from social_auth.exceptions import AuthFailed
+
+
+STEAM_ID = re.compile('steamcommunity.com/openid/id/(.*?)$')
+USER_INFO = 'http://api.steampowered.com/ISteamUser/GetPlayerSummaries/v0002/?'
+
+
+class SteamOpenId(OpenIdAuth):
+    name = 'steam'
+
+    def openid_url(self):
+        """Return Steam OpenID service url"""
+        return 'http://steamcommunity.com/openid'
+
+    def get_user_id(self, details, response):
+        """Return user unique id provided by service"""
+        return self._user_id(response)
+
+    def get_user_details(self, response):
+        user_id = self._user_id(response)
+        url = USER_INFO + urllib.urlencode({
+            'key': self.setting('API_KEY'),
+            'steamids': user_id
+        })
+        details = {}
+        try:
+            player = json.load(self.urlopen(url))
+        except (ValueError, IOError):
+            pass
+        else:
+            if len(player['response']['players']) > 0:
+                player = player['response']['players'][0]
+                details = {'username': player.get('personaname'),
+                           'email': '',
+                           'fullname': '',
+                           'first_name': '',
+                           'last_name': '',
+                           'player': player}
+        return details
+
+    def _user_id(self, response):
+        match = STEAM_ID.search(response.identity_url)
+        if match is None:
+            raise AuthFailed(self, 'Missing Steam Id')
+        return match.group(1)
+
+
+BACKENDS = {
+    'steam': SteamOpenId
+}
