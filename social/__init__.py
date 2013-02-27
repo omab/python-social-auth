c74abba20a28aa293d60d5a76cc7ebfb18dce93d
Matías Aguirre <matiasaguirre@gmail.com>
2013-02-26 22:06:46 -0200

    Mark dev version

--- social/__init__.py
+++ social/__init__.py
@@ -3,4 +3,4 @@ python-social-auth application, allows OpenId or OAuth user
 registration/authentication just adding a few configurations.
 """
 version = (0, 1, 0)
-__version__ = '.'.join(map(str, version))
+__version__ = '.'.join(map(str, version)) + '-dev'
