acab19aa05abe7f17f1a1122c1043b8fdb21e4d7
Matías Aguirre <matiasaguirre@gmail.com>
2013-02-27 01:50:11 -0200

    Fix URLs paths for django app, support setting url prefix (to ease demo setup)

--- examples/django_example/dj/urls.py
+++ examples/django_example/dj/urls.py
@@ -1,8 +1,12 @@
+from django.conf import settings
 from django.conf.urls import patterns, include, url
 
 
+PATH = getattr(settings, 'URL_PATH', '')
+
 urlpatterns = patterns('',
-    url(r'^$', 'example.views.home'),
-    url(r'^done/$', 'example.views.done'),
-    url('', include('social.apps.django_app.urls', namespace='social'))
+    url(r'^%s$' % PATH, 'example.views.home'),
+    url(r'^%sdone/$' % PATH, 'example.views.done', name='done'),
+    url(r'%s' % PATH, include('social.apps.django_app.urls',
+        namespace='social'))
 )
--- examples/django_example/example/views.py
+++ examples/django_example/example/views.py
@@ -1,10 +1,12 @@
 from django.contrib.auth.decorators import login_required
 from django.template import RequestContext
-from django.shortcuts import render_to_response
+from django.shortcuts import render_to_response, redirect
 
 
 def home(request):
     """Home view, displays login mechanism"""
+    if request.user.is_authenticated():
+        return redirect('done')
     return render_to_response('home.html', {}, RequestContext(request))
 
 
