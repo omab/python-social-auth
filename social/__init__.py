3a04828c2c3049ed8b8487eb62ee816f815a1200
Matías Aguirre <matiasaguirre@gmail.com>
2013-02-27 03:33:44 -0200

    Extra bits for versioning

--- social/__init__.py
+++ social/__init__.py
@@ -3,4 +3,5 @@ python-social-auth application, allows OpenId or OAuth user
 registration/authentication just adding a few configurations.
 """
 version = (0, 1, 0)
-__version__ = '.'.join(map(str, version)) + '-dev'
+extra = ''
+__version__ = '.'.join(map(str, version)) + extra
