32ef9c1ee4d77ddc696cd341956aa146ffc16cb6
Matías Aguirre <matiasaguirre@gmail.com>
2013-03-16 01:20:27 -0300

    Initial testings

---
+++ tests/github_test.py
@@ -0,0 +1,65 @@
+import sys
+import json
+import unittest
+
+sys.path.insert(0, '..')
+
+from social.backends.github import GithubOAuth2
+from tests.oauth2_tests import OAuth2Test
+
+
+class GithubTest(OAuth2Test):
+    backend = GithubOAuth2
+    complete_url = '/complete/github/?code=foobar'
+    user_data_url = 'https://api.github.com/user'
+    access_token_body = json.dumps({
+        'access_token': 'foobar',
+        'token_type': 'bearer'
+    })
+    user_data_body = json.dumps({
+        'login': 'octocat',
+        'id': 1,
+        'avatar_url': 'https://github.com/images/error/octocat_happy.gif',
+        'gravatar_id': 'somehexcode',
+        'url': 'https://api.github.com/users/octocat',
+        'name': 'monalisa octocat',
+        'company': 'GitHub',
+        'blog': 'https://github.com/blog',
+        'location': 'San Francisco',
+        'email': 'octocat@github.com',
+        'hireable': False,
+        'bio': 'There once was...',
+        'public_repos': 2,
+        'public_gists': 1,
+        'followers': 20,
+        'following': 0,
+        'html_url': 'https://github.com/octocat',
+        'created_at': '2008-01-14T04:33:35Z',
+        'type': 'User',
+        'total_private_repos': 100,
+        'owned_private_repos': 100,
+        'private_gists': 81,
+        'disk_usage': 10000,
+        'collaborators': 8,
+        'plan': {
+            'name': 'Medium',
+            'space': 400,
+            'collaborators': 10,
+            'private_repos': 20
+        }
+    })
+    settings = {
+        'SOCIAL_AUTH_GITHUB_KEY': 'a-key',
+        'SOCIAL_AUTH_GITHUB_SECRET': 'a-secret-key'
+    }
+    expected_username = 'octocat'
+
+    def test_login(self):
+        self.do_login()
+
+    def test_partial_pipeline(self):
+        self.do_partial_pipeline()
+
+
+if __name__ == '__main__':
+    unittest.main()
---
+++ tests/strategy.py
@@ -0,0 +1,64 @@
+from social.strategies.base import BaseStrategy
+
+
+TEST_URI = 'http://myapp.com'
+TEST_HOST = 'myapp.com'
+
+
+class Redirect(object):
+    def __init__(self, url):
+        self.url = url
+
+
+class TestStrategy(BaseStrategy):
+
+    def __init__(self, *args, **kwargs):
+        self._request_data = {}
+        self._settings = {}
+        self._session = {}
+        super(TestStrategy, self).__init__(*args, **kwargs)
+
+    def redirect(self, url):
+        return Redirect(url)
+
+    def get_setting(self, name):
+        """Return value for given setting name"""
+        return self._settings[name]
+
+    def html(self, content):
+        """Return HTTP response with given content"""
+        return content
+
+    def render_html(self, tpl=None, html=None, context=None):
+        """Render given template or raw html with given context"""
+        return tpl or html
+
+    def request_data(self, merge=True):
+        """Return current request data (POST or GET)"""
+        return self._request_data
+
+    def request_host(self):
+        """Return current host value"""
+        return TEST_HOST
+
+    def session_get(self, name):
+        """Return session value for given key"""
+        return self._session.get(name)
+
+    def session_set(self, name, value):
+        """Set session value for given key"""
+        self._session[name] = value
+
+    def session_pop(self, name):
+        """Pop session value for given key"""
+        return self._session.pop(name, None)
+
+    def build_absolute_uri(self, path=None):
+        """Build absolute URI with given (optional) path"""
+        return TEST_URI + (path or '')
+
+    def set_settings(self, values):
+        self._settings.update(values)
+
+    def set_request_data(self, values):
+        self._request_data.update(values)
