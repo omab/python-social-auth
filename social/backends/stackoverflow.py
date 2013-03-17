09a86cd6f645bfe38aa9917efa1b37aa6f32ba28
Matías Aguirre <matiasaguirre@gmail.com>
2013-03-17 17:20:07 -0300

    Fix stackoverflow backend

--- social/backends/stackoverflow.py
+++ social/backends/stackoverflow.py
@@ -12,13 +12,8 @@ setting, it must be a list of values to request.
 By default account id and token expiration time are stored in extra_data
 field, check OAuthBackend class for details on how to extend it.
 """
-import json
-
-from gzip import GzipFile
-
 from requests import HTTPError
 
-from social.p3 import StringIO
 from social.backends.oauth import BaseOAuth2
 from social.exceptions import AuthUnknownError, AuthCanceled
 
@@ -29,6 +24,7 @@ class StackoverflowOAuth2(BaseOAuth2):
     ID_KEY = 'user_id'
     AUTHORIZATION_URL = 'https://stackexchange.com/oauth'
     ACCESS_TOKEN_URL = 'https://stackexchange.com/oauth/access_token'
+    ACCESS_TOKEN_METHOD = 'POST'
     SCOPE_SEPARATOR = ','
     EXTRA_DATA = [
         ('id', 'id'),
@@ -42,41 +38,15 @@ class StackoverflowOAuth2(BaseOAuth2):
 
     def user_data(self, access_token, *args, **kwargs):
         """Loads user data from service"""
-        opener = self.request('https://api.stackexchange.com/2.1/me', params={
-            'site': 'stackoverflow',
-            'access_token': access_token,
-            'key': self.settings('API_KEY')
-        })
-        if opener.headers.get('content-encoding') == 'gzip':
-            # Stackoverflow doesn't respect no gzip header
-            gzip = GzipFile(fileobj=StringIO(opener.content), mode='r')
-            response = gzip.read()
-        else:
-            response = opener.content
         try:
-            return json.loads(response)['items'][0]
+            return self.get_json(
+                'https://api.stackexchange.com/2.1/me',
+                params={'site': 'stackoverflow',
+                        'access_token': access_token,
+                        'key': self.setting('API_KEY')}
+            )['items'][0]
         except (ValueError, TypeError):
             return None
 
-    def auth_complete(self, *args, **kwargs):
-        """Completes loging process, must return user instance"""
-        self.process_error(self.data)
-        try:
-            response = dict(
-                self.get_querystring(
-                    self.ACCESS_TOKEN_URL,
-                    params=self.auth_complete_params(self.validate_state()),
-                    headers=self.auth_headers()
-                )
-            )
-        except HTTPError as err:
-            if err.response.status_code == 400:
-                raise AuthCanceled(self)
-            else:
-                raise
-        except (ValueError, KeyError):
-            raise AuthUnknownError(self)
-
-        self.process_error(response)
-        return self.do_auth(response['access_token'], response=response,
-                            *args, **kwargs)
+    def request_access_token(self, *args, **kwargs):
+        return self.get_querystring(*args, **kwargs)
