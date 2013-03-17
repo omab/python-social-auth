beca79fc88ecd13c8f4c8e0fb4cbe007ea115bb2
Matías Aguirre <matiasaguirre@gmail.com>
2013-03-17 03:01:04 -0300

    Live tests

---
+++ tests/live_test.py
@@ -0,0 +1,39 @@
+import sys
+import json
+
+sys.path.insert(0, '..')
+
+from social.backends.live import LiveOAuth2
+from tests.oauth2_tests import OAuth2Test
+
+
+class LiveTest(OAuth2Test):
+    backend = LiveOAuth2
+    user_data_url = 'https://apis.live.net/v5.0/me'
+    expected_username = 'FooBar'
+    access_token_body = json.dumps({
+        'access_token': 'foobar',
+        'token_type': 'bearer'
+    })
+    user_data_body = json.dumps({
+        'first_name': 'Foo',
+        'last_name': 'Bar',
+        'name': 'Foo Bar',
+        'locale': 'en_US',
+        'gender': None,
+        'emails': {
+            'personal': None,
+            'account': 'foobar@live.com',
+            'business': None,
+            'preferred': 'foobar@live.com'
+        },
+        'link': 'https://profile.live.com/',
+        'updated_time': '2013-03-17T05:51:30+0000',
+        'id': '1010101010101010'
+    })
+
+    def test_login(self):
+        self.do_login()
+
+    def test_partial_pipeline(self):
+        self.do_partial_pipeline()
