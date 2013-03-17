d848c5782eaf4c0c6f5e67cc1ab8ef84b6e7c2b5
Matías Aguirre <matiasaguirre@gmail.com>
2013-03-17 04:51:01 -0300

    Stocktwits tests

---
+++ tests/stocktwits_test.py
@@ -0,0 +1,39 @@
+import sys
+import json
+
+sys.path.insert(0, '..')
+
+from social.backends.stocktwits import StocktwitsOAuth2
+from tests.oauth2_tests import OAuth2Test
+
+
+class StocktwitsTest(OAuth2Test):
+    backend = StocktwitsOAuth2
+    user_data_url = 'https://api.stocktwits.com/api/2/account/verify.json'
+    expected_username = 'foobar'
+    access_token_body = json.dumps({
+        'access_token': 'foobar',
+        'token_type': 'bearer'
+    })
+    user_data_body = json.dumps({
+        'response': {
+            'status': 200
+        },
+        'user': {
+            'username': 'foobar',
+            'name': 'Foo Bar',
+            'classification': [],
+            'avatar_url': 'http://avatars.stocktwits.net/images/'
+                          'default_avatar_thumb.jpg',
+            'avatar_url_ssl': 'https://s3.amazonaws.com/st-avatars/images/'
+                              'default_avatar_thumb.jpg',
+            'id': 101010,
+            'identity': 'User'
+        }
+    })
+
+    def test_login(self):
+        self.do_login()
+
+    def test_partial_pipeline(self):
+        self.do_partial_pipeline()
