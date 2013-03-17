c5e08d4bcd412cd4931924c67fa5e602cae18194
Matías Aguirre <matiasaguirre@gmail.com>
2013-03-17 15:18:49 -0300

    Yandex tests

---
+++ tests/yandex_test.py
@@ -0,0 +1,31 @@
+import sys
+import json
+
+sys.path.insert(0, '..')
+
+from social.backends.yandex import YandexOAuth2
+from tests.oauth2_tests import OAuth2Test
+
+
+class YandexTest(OAuth2Test):
+    backend = YandexOAuth2
+    user_data_url = 'https://login.yandex.ru/info'
+    expected_username = 'foobar'
+    access_token_body = json.dumps({
+        'access_token': 'foobar',
+        'token_type': 'bearer'
+    })
+    user_data_body = json.dumps({
+        'display_name': 'foobar',
+        'real_name': 'Foo Bar',
+        'sex': None,
+        'id': '101010101',
+        'default_email': 'foobar@yandex.com',
+        'emails': ['foobar@yandex.com']
+    })
+
+    def test_login(self):
+        self.do_login()
+
+    def test_partial_pipeline(self):
+        self.do_partial_pipeline()
