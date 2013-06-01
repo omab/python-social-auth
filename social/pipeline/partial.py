9e6ad6243ff1d9cd80076cad63af99aacb4bbaf9
Matías Aguirre <matiasaguirre@gmail.com>
2013-06-01 06:44:10 -0300

    Added @partial decorator, much simpler than adding entries to pipeline

--- social/pipeline/partial.py
+++ social/pipeline/partial.py
@@ -1,5 +1,18 @@
+from functools import wraps
+
+
 def save_status_to_session(strategy, pipeline_index, *args, **kwargs):
     """Saves current social-auth status to session."""
     strategy.session_set('partial_pipeline',
                          strategy.to_session(pipeline_index + 1,
                                              *args, **kwargs))
+
+
+def partial(func):
+    @wraps(func)
+    def wrapper(strategy, pipeline_index, *args, **kwargs):
+        values = strategy.to_session(pipeline_index, *args, **kwargs)
+        strategy.session_set('partial_pipeline', values)
+        return func(strategy=strategy, pipeline_index=pipeline_index,
+                    *args, **kwargs)
+    return wrapper
