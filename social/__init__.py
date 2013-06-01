9694c17b02c08db40f30fa970551e6de04c40f2c
Matías Aguirre <matiasaguirre@gmail.com>
2013-06-01 06:46:22 -0300

    v0.1.5

--- social/__init__.py
+++ social/__init__.py
@@ -2,6 +2,6 @@
 python-social-auth application, allows OpenId or OAuth user
 registration/authentication just adding a few configurations.
 """
-version = (0, 1, 4)
+version = (0, 1, 5)
 extra = ''
 __version__ = '.'.join(map(str, version)) + extra
