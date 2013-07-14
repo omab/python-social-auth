4b6205c2cc776299a011f2f8d894d054695d8d10
Matías Aguirre <matiasaguirre@gmail.com>
2013-07-14 19:35:04 -0300

    Django admin conf for default django app

--- examples/django_example/dj/settings.py
+++ examples/django_example/dj/settings.py
@@ -73,6 +73,7 @@ TEMPLATE_DIRS = (
 
 INSTALLED_APPS = (
     'django.contrib.auth',
+    'django.contrib.admin',
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.sites',
--- examples/django_example/dj/urls.py
+++ examples/django_example/dj/urls.py
@@ -1,10 +1,14 @@
 from django.conf import settings
 from django.conf.urls import patterns, include, url
+from django.contrib import admin
 
 
 PATH = getattr(settings, 'URL_PATH', '')
 
+admin.autodiscover()
+
 urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
     url(r'^%s$' % PATH, 'example.views.home'),
     url(r'^%sdone/$' % PATH, 'example.views.done', name='done'),
     url(r'%s' % PATH, include('social.apps.django_app.urls',
