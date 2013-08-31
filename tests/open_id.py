053b31f7b83aada98ea2d145c047e1f3ac644470
Matías Aguirre <matiasaguirre@gmail.com>
2013-08-31 20:24:23 -0300

    Move common testing code to base class

--- tests/open_id.py
+++ tests/open_id.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 import sys
 import requests
-import unittest
 from openid import oidutil
 
 PY3 = sys.version_info[0] == 3
@@ -12,14 +11,14 @@ if PY3:
 else:
     from HTMLParser import HTMLParser
 
-from sure import expect
 from httpretty import HTTPretty
 
 sys.path.insert(0, '..')
 
 from social.utils import parse_qs, module_member
-from social.backends.utils import user_backends_data, load_backends
+from social.backends.utils import load_backends
 
+from tests.base import BaseBackendTest
 from tests.models import TestStorage, User, TestUserSocialAuth, TestNonce, \
                          TestAssociation
 from tests.strategy import TestStrategy
@@ -41,7 +40,7 @@ class FormHTMLParser(HTMLParser):
             self.inputs[attrs['name']] = attrs['value']
 
 
-class OpenIdTest(unittest.TestCase):
+class OpenIdTest(BaseBackendTest):
     backend_path = None
     backend = None
     access_token_body = None
@@ -50,11 +49,13 @@ class OpenIdTest(unittest.TestCase):
     expected_username = ''
     settings = None
     partial_login_settings = None
+    raw_complete_url = '/complete/{0}/'
 
     def setUp(self):
         HTTPretty.enable()
         self.backend = module_member(self.backend_path)
-        self.complete_url = '/complete/{0}/'.format(self.backend.name)
+        name = self.backend.name
+        self.complete_url = self.raw_complete_url.format(name)
         self.strategy = TestStrategy(self.backend, TestStorage,
                                      redirect_uri=self.complete_url)
         self.strategy.set_settings({
@@ -108,95 +109,3 @@ class OpenIdTest(unittest.TestCase):
                                status=200,
                                body='is_valid:true\n')
         return self.strategy.complete()
-
-    def do_login(self):
-        user = self.do_start()
-        username = self.expected_username
-        expect(user.username).to.equal(username)
-        expect(self.strategy.session_get('username')).to.equal(username)
-        expect(self.strategy.get_user(user.id)).to.equal(user)
-        expect(self.strategy.backend.get_user(user.id)).to.equal(user)
-        user_backends = user_backends_data(
-            user,
-            self.strategy.get_setting('SOCIAL_AUTH_AUTHENTICATION_BACKENDS'),
-            self.strategy.storage
-        )
-        expect(len(list(user_backends.keys()))).to.equal(3)
-        expect('associated' in user_backends).to.equal(True)
-        expect('not_associated' in user_backends).to.equal(True)
-        expect('backends' in user_backends).to.equal(True)
-        expect(len(user_backends['associated'])).to.equal(1)
-        expect(len(user_backends['not_associated'])).to.equal(1)
-        expect(len(user_backends['backends'])).to.equal(2)
-        return user
-
-    def pipeline_settings(self):
-        self.strategy.set_settings({
-            'SOCIAL_AUTH_PIPELINE': (
-                'social.pipeline.partial.save_status_to_session',
-                'tests.pipeline.ask_for_password',
-                'tests.pipeline.ask_for_slug',
-                'social.pipeline.social_auth.social_user',
-                'social.pipeline.user.get_username',
-                'social.pipeline.user.create_user',
-                'social.pipeline.social_auth.associate_user',
-                'social.pipeline.social_auth.load_extra_data',
-                'tests.pipeline.set_password',
-                'tests.pipeline.set_slug',
-                'social.pipeline.user.user_details'
-            )
-        })
-
-    def pipeline_handlers(self, url):
-        HTTPretty.register_uri(HTTPretty.GET, url, status=200, body='foobar')
-        HTTPretty.register_uri(HTTPretty.POST, url, status=200)
-
-    def pipeline_password_handling(self, url):
-        password = 'foobar'
-        requests.get(url)
-        requests.post(url, data={'password': password})
-
-        data = parse_qs(HTTPretty.last_request.body)
-        expect(data['password']).to.equal(password)
-        self.strategy.session_set('password', data['password'])
-        return password
-
-    def pipeline_slug_handling(self, url):
-        slug = 'foo-bar'
-        requests.get(url)
-        requests.post(url, data={'slug': slug})
-
-        data = parse_qs(HTTPretty.last_request.body)
-        expect(data['slug']).to.equal(slug)
-        self.strategy.session_set('slug', data['slug'])
-        return slug
-
-    def do_partial_pipeline(self):
-        url = self.strategy.build_absolute_uri('/password')
-        self.pipeline_settings()
-        redirect = self.do_start()
-        expect(redirect.url).to.equal(url)
-        self.pipeline_handlers(url)
-        password = self.pipeline_password_handling(url)
-
-        data = self.strategy.session_pop('partial_pipeline')
-        idx, backend, xargs, xkwargs = self.strategy.from_session(data)
-        expect(backend).to.equal(self.backend.name)
-        redirect = self.strategy.continue_pipeline(pipeline_index=idx,
-                                                   *xargs, **xkwargs)
-
-        url = self.strategy.build_absolute_uri('/slug')
-        expect(redirect.url).to.equal(url)
-        self.pipeline_handlers(url)
-        slug = self.pipeline_slug_handling(url)
-
-        data = self.strategy.session_pop('partial_pipeline')
-        idx, backend, xargs, xkwargs = self.strategy.from_session(data)
-        expect(backend).to.equal(self.backend.name)
-        user = self.strategy.continue_pipeline(pipeline_index=idx,
-                                               *xargs, **xkwargs)
-
-        expect(user.username).to.equal(self.expected_username)
-        expect(user.slug).to.equal(slug)
-        expect(user.password).to.equal(password)
-        return user
