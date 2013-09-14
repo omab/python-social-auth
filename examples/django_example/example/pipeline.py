775c11a500e75d4309215d8a626713f14a224903
Matías Aguirre <matiasaguirre@gmail.com>
2013-09-14 18:27:16 -0300

    Remove prints

--- examples/django_example/example/pipeline.py
+++ examples/django_example/example/pipeline.py
@@ -5,14 +5,8 @@ from social.pipeline.partial import partial
 
 @partial
 def require_email(strategy, details, *args, **kwargs):
-    print "REQUIRE EMAIL 0"
     if not details.get('email'):
-        print "REQUIRE EMAIL 1: no email"
         if strategy.session_get('saved_email'):
-            print "REQUIRE EMAIL 2: got email:", \
-                  strategy.session_get('saved_email')
             details['email'] = strategy.session_pop('saved_email')
         else:
-            print "REQUIRE EMAIL 3: redirect"
             return redirect('require_email')
-    print "DETAILS:", details
