076dfcae4cedf95780bb008db931b5e57b2ff1f2
Matías Aguirre <matiasaguirre@gmail.com>
2013-09-21 19:21:32 -0300

    Drop password

--- social/pipeline/user.py
+++ social/pipeline/user.py
@@ -3,7 +3,7 @@ from uuid import uuid4
 from social.utils import slugify, module_member
 
 
-USER_FIELDS = ['username', 'email', 'password']
+USER_FIELDS = ['username', 'email']
 
 
 def get_username(strategy, details, user=None, *args, **kwargs):
