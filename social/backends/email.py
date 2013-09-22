7c93de2ac1f462e6bdf1f830be606ded80c97ac6
Matías Aguirre <matiasaguirre@gmail.com>
2013-09-22 13:27:55 -0300

    Move common code to base class

--- social/backends/email.py
+++ social/backends/email.py
@@ -1,47 +1,7 @@
-from social.backends.base import BaseAuth
-from social.exceptions import AuthMissingParameter
+from social.backends.legacy import LegacyAuth
 
 
-class EmailAuth(BaseAuth):
+class EmailAuth(LegacyAuth):
     name = 'email'
+    ID_KEY = 'email'
     REQUIRES_EMAIL_VALIDATION = True
-
-    def get_user_id(self, details, response):
-        return details['email']
-
-    def get_user_details(self, response):
-        """Return user details"""
-        email = response['email']
-        fullname = response.get('fullname', '')
-        first_name = response.get('first_name', '')
-        last_name = response.get('last_name', '')
-        if fullname and not (first_name or last_name):
-            try:
-                first_name, last_name = fullname.split(' ', 1)
-            except ValueError:
-                first_name = fullname
-                last_name = last_name or ''
-        return {
-            'username': email.split('@', 1)[0],
-            'email': email,
-            'fullname': fullname,
-            'first_name': first_name,
-            'last_name': last_name
-        }
-
-    def auth_url(self):
-        return self.setting('FORM_URL')
-
-    def auth_html(self):
-        return self.strategy.render_html(tpl=self.setting('FORM_HTML'))
-
-    def uses_redirect(self):
-        return self.setting('FORM_URL') and not \
-               self.setting('FORM_HTML')
-
-    def auth_complete(self, *args, **kwargs):
-        """Completes loging process, must return user instance"""
-        if 'email' not in self.data:
-            raise AuthMissingParameter(self, 'email')
-        kwargs.update({'response': self.data, 'backend': self})
-        return self.strategy.authenticate(*args, **kwargs)
---
+++ social/backends/legacy.py
@@ -0,0 +1,48 @@
+from social.backends.base import BaseAuth
+from social.exceptions import AuthMissingParameter
+
+
+class LegacyAuth(BaseAuth):
+    def get_user_id(self, details, response):
+        return details.get(self.ID_KEY) or \
+               response.get(self.ID_KEY)
+
+    def auth_url(self):
+        return self.setting('FORM_URL')
+
+    def auth_html(self):
+        return self.strategy.render_html(tpl=self.setting('FORM_HTML'))
+
+    def uses_redirect(self):
+        return self.setting('FORM_URL') and not \
+               self.setting('FORM_HTML')
+
+    def auth_complete(self, *args, **kwargs):
+        """Completes loging process, must return user instance"""
+        if self.ID_KEY not in self.data:
+            raise AuthMissingParameter(self, self.ID_KEY)
+        kwargs.update({'response': self.data, 'backend': self})
+        return self.strategy.authenticate(*args, **kwargs)
+
+    def get_user_details(self, response):
+        """Return user details"""
+        email = response.get('email', '')
+        username = response.get('username', '')
+        fullname = response.get('fullname', '')
+        first_name = response.get('first_name', '')
+        last_name = response.get('last_name', '')
+        if fullname and not (first_name or last_name):
+            try:
+                first_name, last_name = fullname.split(' ', 1)
+            except ValueError:
+                first_name = fullname
+                last_name = last_name or ''
+        if email and not username:
+            username = email.split('@', 1)[0]
+        return {
+            'username': username,
+            'email': email,
+            'fullname': fullname,
+            'first_name': first_name,
+            'last_name': last_name
+        }
