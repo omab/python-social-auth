967cfaca52c64878cc08b710dddac0c64259bce2
Matías Aguirre <matiasaguirre@gmail.com>
2013-09-22 13:27:41 -0300

    Small improve to email partial pipeline on example app

--- examples/django_example/example/pipeline.py
+++ examples/django_example/example/pipeline.py
@@ -4,8 +4,10 @@ from social.pipeline.partial import partial
 
 
 @partial
-def require_email(strategy, details, *args, **kwargs):
-    if not details.get('email'):
+def require_email(strategy, details, user=None, is_new=False, *args, **kwargs):
+    if user and user.email:
+        return
+    elif is_new and not details.get('email'):
         if strategy.session_get('saved_email'):
             details['email'] = strategy.session_pop('saved_email')
         else:
