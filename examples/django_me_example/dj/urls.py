76ee54564c162ec233bab99113daa07859c5225e
Matías Aguirre <matiasaguirre@gmail.com>
2013-09-22 21:00:30 -0300

    Mongoengine example

---
+++ examples/django_me_example/dj/urls.py
@@ -0,0 +1,20 @@
+from django.conf import settings
+from django.conf.urls import patterns, include, url
+from django.contrib import admin
+
+
+PATH = getattr(settings, 'URL_PATH', '')
+
+admin.autodiscover()
+
+urlpatterns = patterns('',
+    url(r'^admin/', include(admin.site.urls)),
+    url(r'^signup-email/', 'example.views.signup_email'),
+    url(r'^email-sent/', 'example.views.validation_sent'),
+    url(r'^%s$' % PATH, 'example.views.home'),
+    url(r'^%sdone/$' % PATH, 'example.views.done', name='done'),
+    url(r'^%semail/$' % PATH, 'example.views.require_email',
+        name='require_email'),
+    url(r'%s' % PATH, include('social.apps.django_app.urls',
+        namespace='social'))
+)
