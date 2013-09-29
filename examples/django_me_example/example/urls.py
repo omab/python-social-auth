1b3f75f2e1edb40573b66bf7a00f1f4daedd4947
Matías Aguirre <matiasaguirre@gmail.com>
2013-09-29 14:16:43 -0300

    Remove path from urls

--- examples/django_me_example/example/urls.py
+++ examples/django_me_example/example/urls.py
@@ -1,21 +1,16 @@
-from django.conf import settings
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
 
 
-PATH = getattr(settings, 'URL_PATH', '')
-
 admin.autodiscover()
 
 urlpatterns = patterns('',
+    url(r'^$', 'example.app.views.home'),
     url(r'^admin/', include(admin.site.urls)),
     url(r'^signup-email/', 'example.app.views.signup_email'),
     url(r'^email-sent/', 'example.app.views.validation_sent'),
-    url(r'^%s$' % PATH, 'example.app.views.home'),
-    url(r'^login/$' % PATH, 'example.app.views.home'),
-    url(r'^%sdone/$' % PATH, 'example.app.views.done', name='done'),
-    url(r'^%semail/$' % PATH, 'example.app.views.require_email',
-        name='require_email'),
-    url(r'%s' % PATH, include('social.apps.django_app.urls',
-        namespace='social'))
+    url(r'^login/$', 'example.app.views.home'),
+    url(r'^done/$', 'example.app.views.done', name='done'),
+    url(r'^email/$', 'example.app.views.require_email', name='require_email'),
+    url(r'', include('social.apps.django_app.urls', namespace='social'))
 )
