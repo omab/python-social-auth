c4306a7c66ce567c2911566555f9f3bffda931b7
Jichao Ouyang <oyanglulu@gmail.com>
2014-01-07 17:41:24 +0800

    add to django example

--- social/backends/taobao.py
+++ social/backends/taobao.py
@@ -2,64 +2,40 @@ import urllib2,urllib
 from urllib2 import Request, urlopen, HTTPError
 from urllib import urlencode
 from urlparse import urlsplit
-from social_auth.backends.exceptions import StopPipeline, AuthException, \
-                                            AuthFailed, AuthCanceled, \
-                                            AuthUnknownError, AuthTokenError, \
-                                            AuthMissingParameter
-from django.conf import settings
-from django.utils import simplejson
 import json
-from django.contrib.auth import authenticate
-from social_auth.backends import BaseOAuth2, OAuthBackend, USERNAME
+from social.exceptions import AuthFailed
+from social.backends.oauth import BaseOAuth2
 # taobao OAuth base configuration
 TAOBAO_OAUTH_HOST = 'oauth.taobao.com'
 # TAOBAO_OAUTH_ROOT = 'authorize'
 #Always use secure connection
-TAOBAO_OAUTH_REQUEST_TOKEN_URL = 'https://%s/request_token' % (TAOBAO_OAUTH_HOST)
 TAOBAO_OAUTH_AUTHORIZATION_URL = 'https://%s/authorize' % (TAOBAO_OAUTH_HOST)
 TAOBAO_OAUTH_ACCESS_TOKEN_URL = 'https://%s/token' % (TAOBAO_OAUTH_HOST)
 
 TAOBAO_CHECK_AUTH = 'https://eco.taobao.com/router/rest'
-TAOBAO_USER_SHOW = 'https://%s/user/get_user_info' % TAOBAO_OAUTH_HOST
-class TAOBAOBackend(OAuthBackend):
-    """Taobao OAuth authentication backend"""
-    name = 'taobao'
-
-    def get_user_id(self, details, response):
-        return response['taobao_user_id']
-    
-    def get_user_details(self, response):
-        """Return user details from Taobao account"""
-        
-        username = response['taobao_user_nick']
-        return {USERNAME: username,
-                'email': '',  # not supplied
-                'last_name': ''}
         
 class TAOBAOAuth(BaseOAuth2):
     """Taobao OAuth authentication mechanism"""
+    name="taobao"
+    ID_KEY='taobao_user_id'
     AUTHORIZATION_URL = TAOBAO_OAUTH_AUTHORIZATION_URL
-    REQUEST_TOKEN_URL = TAOBAO_OAUTH_REQUEST_TOKEN_URL
     ACCESS_TOKEN_URL = TAOBAO_OAUTH_ACCESS_TOKEN_URL
-    SERVER_URL = TAOBAO_OAUTH_HOST
-    AUTH_BACKEND = TAOBAOBackend
-    SETTINGS_KEY_NAME = 'TAOBAO_CONSUMER_KEY'
-    SETTINGS_SECRET_NAME = 'TAOBAO_CONSUMER_SECRET'
    
-    def user_data(self, access_token):
+    def user_data(self, access_token, *args, **kwargs):
         """Return user data provided"""
         params = {'method':'taobao.user.get',
                   'fomate':'json',
                   'v':'2.0',
                   'access_token': access_token}
-        
-        url = TAOBAO_CHECK_AUTH + urllib.urlencode(params)
-        # # print url
-       
         try:
-            return simplejson.load(urllib.urlopen(url))
+            return self.get_json(TAOBAO_CHECK_AUTH, params=params)
         except ValueError:
             return None
+
+    def get_user_details(self, response):
+        """Return user details from Taobao account"""
+        username = response.get('taobao_user_nick')
+        return {'username': username}
 
 # Backend definition
 BACKENDS = {
