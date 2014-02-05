711b892c94efabf2784157107b7ae5e122b21b12
Matías Aguirre <matiasaguirre@gmail.com>
2014-02-05 20:29:25 -0200

    Fix iexact field lookup. Refs #179

--- social/storage/django_orm.py
+++ social/storage/django_orm.py
@@ -67,7 +67,7 @@ class DjangoUserMixin(UserMixin):
 
     @classmethod
     def get_users_by_email(cls, email):
-        return cls.user_model().objects.filter(email_iexact=email)
+        return cls.user_model().objects.filter(email__iexact=email)
 
     @classmethod
     def get_social_auth(cls, provider, uid):
