7ff6ebf94973f3a12ce46ebf4eb409d67f17ca9e
Auston Bunsen <auston.bunsen@gmail.com>
2014-03-15 17:07:47 -0400

    added strava support!

---
+++ social/backends/strava.py
@@ -0,0 +1,30 @@
+"""
+Strava OAuth2 backend, docs at:
+    http://psa.matiasaguirre.net/docs/backends/strava.html
+"""
+from social.backends.oauth import BaseOAuth2
+
+
+class StravaOAuth(BaseOAuth2):
+    name = 'strava'
+    AUTHORIZATION_URL = 'https://www.strava.com/oauth/authorize'
+    ACCESS_TOKEN_URL = 'https://www.strava.com/oauth/token'
+    ACCESS_TOKEN_METHOD = 'POST'
+
+    def get_user_id(self, details, response):
+        return response['athlete']['id']
+
+    def get_user_details(self, response):
+        """Return user details from Strava account"""
+        username = response['athlete']['id'] # because there is no usernames on strava
+        fullname = response['athlete'].get('first_name', '')
+        fullname += ' %s' % response['athlete'].get('lasst_name', '')
+        email = response['athlete'].get('email', '')
+        return {'username': username,
+                'first_name': fullname,
+                'email': email}
+
+    def user_data(self, access_token, *args, **kwargs):
+        """Loads user data from service"""
+        return self.get_json('https://www.strava.com/api/v3/athlete',
+                             params={'access_token': access_token})
