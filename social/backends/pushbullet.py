52e09460bb44d6680e0c7c5a3e7bc11f0f3aa054
Matías Aguirre <matiasaguirre@gmail.com>
2014-08-16 00:02:04 -0300

    PEP8

--- social/backends/pushbullet.py
+++ social/backends/pushbullet.py
@@ -1,5 +1,7 @@
-from social.backends.oauth import BaseOAuth2
 import base64
+
+from social.backends.oauth import BaseOAuth2
+
 
 class PushbulletOAuth2(BaseOAuth2):
     """pushbullet OAuth authentication backend"""
@@ -11,21 +13,11 @@ class PushbulletOAuth2(BaseOAuth2):
     ACCESS_TOKEN_URL = 'https://api.pushbullet.com/oauth2/token'
     ACCESS_TOKEN_METHOD = 'POST'
     STATE_PARAMETER = False
-    RESPONSE_TYPE = "code"
 
     def get_user_details(self, response):
         return {'username': response.get('access_token')}
-    
-    def get_user_id(self, details, response):
-    	#return details.get(details['iden']);
-        return self.get_json('https://api.pushbullet.com/v2/users/me',
-                            params={}, headers={'Authorization': "Basic "+base64.b64encode(details['username'])})['iden']
-        
-        
-    # def user_data(self, access_token, *args, **kwargs):
-    #     """Return user data provided"""
-    #     return self.get_json('https://api.pushbullet.com/v2/users/me',
-    #                         params={}, headers={'Authorization': "Basic "+base64.b64encode(access_token)})
 
-        #'access_token': access_token
-		#a.eyJ0b2tlbiI6ICJ1akRvanRGSUUwV3Rqelp1SzlKbUhBIiwgInIiOiAicVVHUXBxRUdjMU9Pa2E0M3YyZEpRUjNQVXhpNWZDNFEiLCAidCI6ICJncmFudF92MyJ9.w_1dpcSUVzto1UByzY0901p0d7KSKv5_i1ESmmeH6ww
+    def get_user_id(self, details, response):
+        auth = 'Basic {0}'.format(base64.b64encode(details['username']))
+        return self.get_json('https://api.pushbullet.com/v2/users/me',
+                             headers={'Authorization': auth})['iden']
