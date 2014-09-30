cc5d75dac91a4520d52973580afe17829b8b9a55
Matías Aguirre <matiasaguirre@gmail.com>
2014-09-30 11:07:52 -0300

    PEP8

--- social/apps/django_app/default/migrations/0001_initial.py
+++ social/apps/django_app/default/migrations/0001_initial.py
@@ -18,7 +18,8 @@ class Migration(migrations.Migration):
             name='Association',
             fields=[
                 ('id', models.AutoField(
-                    verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                    verbose_name='ID', serialize=False, auto_created=True,
+                    primary_key=True)),
                 ('server_url', models.CharField(max_length=255)),
                 ('handle', models.CharField(max_length=255)),
                 ('secret', models.CharField(max_length=255)),
@@ -30,13 +31,15 @@ class Migration(migrations.Migration):
                 'db_table': 'social_auth_association',
             },
             bases=(
-                models.Model, social.storage.django_orm.DjangoAssociationMixin),
+                models.Model, social.storage.django_orm.DjangoAssociationMixin
+            ),
         ),
         migrations.CreateModel(
             name='Code',
             fields=[
                 ('id', models.AutoField(
-                    verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                    verbose_name='ID', serialize=False, auto_created=True,
+                    primary_key=True)),
                 ('email', models.EmailField(max_length=75)),
                 ('code', models.CharField(max_length=32, db_index=True)),
                 ('verified', models.BooleanField(default=False)),
@@ -50,7 +53,8 @@ class Migration(migrations.Migration):
             name='Nonce',
             fields=[
                 ('id', models.AutoField(
-                    verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                    verbose_name='ID', serialize=False, auto_created=True,
+                    primary_key=True)),
                 ('server_url', models.CharField(max_length=255)),
                 ('timestamp', models.IntegerField()),
                 ('salt', models.CharField(max_length=65)),
@@ -64,7 +68,8 @@ class Migration(migrations.Migration):
             name='UserSocialAuth',
             fields=[
                 ('id', models.AutoField(
-                    verbose_name='ID', serialize=False, auto_created=True, primary_key=True)),
+                    verbose_name='ID', serialize=False, auto_created=True,
+                    primary_key=True)),
                 ('provider', models.CharField(max_length=32)),
                 ('uid', models.CharField(max_length=255)),
                 ('extra_data', social.apps.django_app.default.fields.JSONField(
