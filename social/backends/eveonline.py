6dcb3dd44b4bfa2559002a95295ab254e3c0f110
Matías Aguirre <matiasaguirre@gmail.com>
2015-03-07 22:24:57 -0200

    PEP8, quotes and extra_data

--- social/backends/eveonline.py
+++ social/backends/eveonline.py
@@ -2,9 +2,6 @@
 EVE Online Single Sign-On (SSO) OAuth2 backend
 Documentation at https://developers.eveonline.com/resource/single-sign-on
 """
-from requests import HTTPError
-
-from social.exceptions import AuthFailed
 from social.backends.oauth import BaseOAuth2
 
 
@@ -13,27 +10,32 @@ class EVEOnlineOAuth2(BaseOAuth2):
     name = 'eveonline'
     AUTHORIZATION_URL = 'https://login.eveonline.com/oauth/authorize'
     ACCESS_TOKEN_URL = 'https://login.eveonline.com/oauth/token'
-    ID_KEY = "CharacterID"
+    ID_KEY = 'CharacterID'
     ACCESS_TOKEN_METHOD = 'POST'
     EXTRA_DATA = [
         ('CharacterID', 'id'),
+        ('ExpiresOn', 'expires'),
+        ('CharacterOwnerHash', 'owner_hash', True),
         ('refresh_token', 'refresh_token', True),
-        ('expires_in', 'expires'),
     ]
 
     def get_user_details(self, response):
         """Return user details from EVE Online account"""
         user_data = self.user_data(response['access_token'])
-        print user_data
-        fullname, first_name, last_name = self.get_user_names(user_data['CharacterName'])
-        return {'username': fullname,
-                'fullname': fullname,
-                'first_name': first_name,
-                'last_name': last_name}
+        fullname, first_name, last_name = self.get_user_names(
+            user_data['CharacterName']
+        )
+        return {
+            'email': '',
+            'username': fullname,
+            'fullname': fullname,
+            'first_name': first_name,
+            'last_name': last_name
+        }
 
     def user_data(self, access_token, *args, **kwargs):
         """Get Character data from EVE server"""
         return self.get_json(
             'https://login.eveonline.com/oauth/verify',
-            headers={'Authorization': "Bearer {0}".format(access_token)}
+            headers={'Authorization': 'Bearer {0}'.format(access_token)}
         )
