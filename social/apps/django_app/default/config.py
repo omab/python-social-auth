b51629fcc54ad755f1b17f27712aacc860f34129
DanielJDufour <daniel.j.dufour@gmail.com>
2015-03-12 20:47:26 -0400

    update for django 1.9

--- social/apps/django_app/default/config.py
+++ social/apps/django_app/default/config.py
@@ -4,3 +4,10 @@ from django.apps import AppConfig
 class PythonSocialAuthConfig(AppConfig):
     name = 'social.apps.django_app.default'
     verbose_name = 'Python Social Auth'
+
+    def ready(self):
+        from social.strategies.utils import set_current_strategy_getter
+        from social.apps.django_app.utils import load_strategy
+        set_current_strategy_getter(load_strategy)
+
+
