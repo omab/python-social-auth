a8b2790b595a19949a7157c3103fd64d75b93f7f
Matías Aguirre <matiasaguirre@gmail.com>
2015-04-15 11:56:43 -0300

    Flag dev version

--- social/__init__.py
+++ social/__init__.py
@@ -2,6 +2,6 @@
 python-social-auth application, allows OpenId or OAuth user
 registration/authentication just adding a few configurations.
 """
-version = (0, 2, 6)
-extra = ''
+version = (0, 2, 7)
+extra = '-dev'
 __version__ = '.'.join(map(str, version)) + extra
