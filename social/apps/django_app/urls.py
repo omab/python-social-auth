1851b5faeaa84a6544f584719104c0b2534e79a5
Matías Aguirre <matiasaguirre@gmail.com>
2015-04-21 20:29:31 -0300

    Make URLs trailing slash be configurable by setting. Refs #505

--- social/apps/django_app/urls.py
+++ social/apps/django_app/urls.py
@@ -1,4 +1,5 @@
 """URLs module"""
+from django.conf import settings
 try:
     from django.conf.urls import patterns, url
 except ImportError:
@@ -6,15 +7,21 @@ except ImportError:
     from django.conf.urls.defaults import patterns, url
 
 
+from social.utils import setting_name
+
+
+extra = getattr(settings, setting_name('TRAILING_SLASH'), True) and '/' or ''
+
+
 urlpatterns = patterns('social.apps.django_app.views',
     # authentication / association
-    url(r'^login/(?P<backend>[^/]+)/?$', 'auth',
+    url(r'^login/(?P<backend>[^/]+){0}$'.format(extra), 'auth',
         name='begin'),
-    url(r'^complete/(?P<backend>[^/]+)/?$', 'complete',
+    url(r'^complete/(?P<backend>[^/]+){0}$'.format(extra), 'complete',
         name='complete'),
     # disconnection
-    url(r'^disconnect/(?P<backend>[^/]+)/?$', 'disconnect',
+    url(r'^disconnect/(?P<backend>[^/]+){0}$'.format(extra), 'disconnect',
         name='disconnect'),
-    url(r'^disconnect/(?P<backend>[^/]+)/(?P<association_id>[^/]+)/?$',
-        'disconnect', name='disconnect_individual'),
+    url(r'^disconnect/(?P<backend>[^/]+)/(?P<association_id>[^/]+){0}$'
+            .format(extra), 'disconnect', name='disconnect_individual'),
 )
