7b510e4ae26532123670e0be8c73be31b4832ba3
vinhub <vin.bhalerao@gmail.com>
2015-05-15 10:47:26 -0700

    updated test cases

--- social/tests/backends/test_azuread.py
+++ social/tests/backends/test_azuread.py
@@ -1,6 +1,8 @@
 import json
 from social.p3 import urlencode
 from social.tests.backends.oauth import OAuth2Test
+from httpretty import HTTPretty
+from social.tests.models import User
 
 class AzureADOAuth2Test(OAuth2Test):
 
@@ -18,13 +20,16 @@ class AzureADOAuth2Test(OAuth2Test):
                     '4YjgxLTQzOGMtOGQ0ZS1hZDZmNTYyYjI4YmIiLCJ1cG4iOiJmb29iYXJAdGVzdC5vbm1pY3Jvc29mdC5jb20iLCJnaXZlbl9u'
                     'YW1lIjoiZm9vIiwiZmFtaWx5X25hbWUiOiJiYXIiLCJuYW1lIjoiZm9vIGJhciIsInVuaXF1ZV9uYW1lIjoiZm9vYmFyQHRlc'
                     '3Qub25taWNyb3NvZnQuY29tIiwicHdkX2V4cCI6IjQ3MzMwOTY4IiwicHdkX3VybCI6Imh0dHBzOi8vcG9ydGFsLm1pY3Jvc2'
-                    '9mdG9ubGluZS5jb20vQ2hhbmdlUGFzc3dvcmQuYXNweCJ9.3V50dHXTZOHj9UWtkn2g7BjX5JxNe8skYlK4PdhiLz4'
+                    '9mdG9ubGluZS5jb20vQ2hhbmdlUGFzc3dvcmQuYXNweCJ9.3V50dHXTZOHj9UWtkn2g7BjX5JxNe8skYlK4PdhiLz4',
+        'expires_in': 3600,
+        'expires_on': 1423650396,
+        'not_before': 1423646496
     })
 
     refresh_token_body = json.dumps({
         'access_token': 'foobar-new-token',
         'token_type': 'bearer',
-        'expires_in': 3600.0,
+        'expires_in': 3600,
         'refresh_token': 'foobar-new-refresh-token',
         'scope': 'identity'
     })
@@ -38,3 +43,16 @@ class AzureADOAuth2Test(OAuth2Test):
     def test_refresh_token(self):
         user, social = self.do_refresh_token()
         self.assertEqual(social.extra_data['access_token'], 'foobar-new-token')
+
+    # TODO:
+    # def test_get_access_token(self):
+        # self.do_login()
+        # HTTPretty.register_uri(self._method(self.backend.REFRESH_TOKEN_METHOD),
+                               # self.backend.REFRESH_TOKEN_URL or
+                               # self.backend.ACCESS_TOKEN_URL,
+                               # status=200,
+                               # body=self.refresh_token_body)
+        # user = list(User.cache.values())[0]
+        # token = self.backend.get_auth_token(user_id=user.id)
+        # self.assertEqual(token, 'foobar-new-token')
+        
