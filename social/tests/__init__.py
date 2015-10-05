7f74771b02cbb470e1c5d086f8a72fdb61dc3694
Matías Aguirre <matiasaguirre@gmail.com>
2015-10-05 10:34:58 -0300

    Fix syntax error

--- social/tests/__init__.py
+++ social/tests/__init__.py
@@ -4,6 +4,6 @@ import warnings
 
 # Ignore deprecation warnings on Python2.6. Maybe it's time to ditch this
 # oldie?
-if sys.version_info[0] == 2 and sys.version_info[1] == 6 or
+if sys.version_info[0] == 2 and sys.version_info[1] == 6 or \
    hasattr(sys, 'pypy_version_info'):
     warnings.filterwarnings('ignore', category=Warning)
