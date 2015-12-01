97b701cba23465ab495530c694360c0857dc7595
Facundo Olano <facundo.olano@gmail.com>
2015-12-01 18:53:50 -0300

    Add code model to 0002 migration

--- social/apps/django_app/default/migrations/0002_add_related_name.py
+++ social/apps/django_app/default/migrations/0002_add_related_name.py
@@ -2,7 +2,9 @@
 from __future__ import unicode_literals
 
 from django.db import models, migrations
+import social.apps.django_app.default.fields
 from django.conf import settings
+import social.storage.django_orm
 
 
 class Migration(migrations.Migration):
@@ -12,6 +14,25 @@ class Migration(migrations.Migration):
     ]
 
     operations = [
+        migrations.CreateModel(
+            name='Code',
+            fields=[
+                ('id', models.AutoField(
+                    verbose_name='ID', serialize=False, auto_created=True,
+                    primary_key=True)),
+                ('email', models.EmailField(max_length=75)),
+                ('code', models.CharField(max_length=32, db_index=True)),
+                ('verified', models.BooleanField(default=False)),
+            ],
+            options={
+                'db_table': 'social_auth_code',
+            },
+            bases=(models.Model, social.storage.django_orm.DjangoCodeMixin),
+        ),
+        migrations.AlterUniqueTogether(
+            name='code',
+            unique_together=set([('email', 'code')]),
+        ),
         migrations.AlterField(
             model_name='usersocialauth',
             name='user',
