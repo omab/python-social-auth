8ccc7326fb568042c22d539fad8e50bb7d73b1f1
hellvix <elvisafc@gmail.com>
2016-02-23 14:19:26 -0300

    Update base.py
    Fixes exception 'str' object has no attribute 'update' when user profile is updated.

--- social/storage/base.py
+++ social/storage/base.py
@@ -81,7 +81,7 @@ class UserMixin(object):
 
     def set_extra_data(self, extra_data=None):
         if extra_data and self.extra_data != extra_data:
-            if self.extra_data:
+            if self.extra_data and not isinstance(self.extra_data, str):
                 self.extra_data.update(extra_data)
             else:
                 self.extra_data = extra_data
