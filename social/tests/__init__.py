8f8de02d7c6912847b8b092940ab957f5bf0a1d6
Matías Aguirre <matiasaguirre@gmail.com>
2016-04-15 13:29:15 -0300

    Ditch Python 2.6, bump up requirements

--- social/tests/__init__.py
+++
@@ -1,9 +0,0 @@
-import sys
-import warnings
-
-
-# Ignore deprecation warnings on Python2.6. Maybe it's time to ditch this
-# oldie?
-if sys.version_info[0] == 2 and sys.version_info[1] == 6 or \
-   hasattr(sys, 'pypy_version_info'):
-    warnings.filterwarnings('ignore', category=Warning)
