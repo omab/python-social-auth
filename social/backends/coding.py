93eb2a1b78166695a00ad56021182ffa4f33e859
Matías Aguirre <matiasaguirre@gmail.com>
2016-06-04 22:12:14 -0300

    PEP8

--- social/backends/coding.py
+++ social/backends/coding.py
@@ -41,5 +41,8 @@ class CodingOAuth2(BaseOAuth2):
         return data.get('data')
 
     def _user_data(self, access_token, path=None):
-        url = urljoin(self.api_url(), 'account/current_user{0}'.format(path or ''))
+        url = urljoin(
+            self.api_url(),
+            'account/current_user{0}'.format(path or '')
+        )
         return self.get_json(url, params={'access_token': access_token})
