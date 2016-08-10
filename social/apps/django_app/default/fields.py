omab/python-social-auth

commit 10a6f5570a4471d156c2bc06ecd4313189d23172

Fix 'RemovedInDjango110Warning: SubfieldBase has been deprecated.' messages from Django 1.9.

social/apps/django_app/default/fields.py
@@ -11,7 +11,7 @@ except ImportError:
     from django.utils.encoding import smart_text
 
 
-class JSONField(six.with_metaclass(models.SubfieldBase, models.TextField)):
+class JSONField(models.TextField):
     """Simple JSON field that stores python structures as JSON strings
     on database.
     """

commit 03ae00154900a8150a5bf941663425698aacdb6b

Fixed Django < 1.8 broken compatibility
This commit 10a6f5570a4471d156c2bc06ecd4313189d23172 broke compatible with Django < 1.8, fixed

social/apps/django_app/default/fields.py
@@ -1,5 +1,6 @@
 import json
 import six
+import functools
 
 from django.core.exceptions import ValidationError
 from django.db import models
@@ -10,8 +11,14 @@ try:
 except ImportError:
     from django.utils.encoding import smart_text
 
+try:
+    from django.db.models import SubfieldBase
+    field_class = functools.partial(six.with_metaclass, SubfieldBase)
+except ImportError:
+    field_class = functools.partial(six.with_metaclass, type)
 
-class JSONField(models.TextField):
+
+class JSONField(field_class(models.TextField)):
     """Simple JSON field that stores python structures as JSON strings
     on database.
     """