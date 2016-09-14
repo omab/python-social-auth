cd4e8044ba6522900e3ad8d0cba52201989574b6
Semyon Pupkov <semen.pupkov@gmail.com>
2016-09-15 00:05:10 +0500

    Add ON DELETE CASCADE for user fk

--- social/apps/django_app/default/models.py
+++ social/apps/django_app/default/models.py
@@ -30,7 +30,7 @@ ASSOCIATION_HANDLE_LENGTH = getattr(
 
 class AbstractUserSocialAuth(models.Model, DjangoUserMixin):
     """Abstract Social Auth association model"""
-    user = models.ForeignKey(USER_MODEL, related_name='social_auth')
+    user = models.ForeignKey(USER_MODEL, related_name='social_auth', on_delete=models.CASCADE)
     provider = models.CharField(max_length=32)
     uid = models.CharField(max_length=UID_LENGTH)
     extra_data = JSONField()
