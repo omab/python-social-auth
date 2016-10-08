8372117b0cce63744558b0d1e78f439aeab895f4
iampark <jeesoo@gmail.com>
2016-10-08 00:52:01 -0700

    Update test_lyft.py

--- social/tests/backends/test_lyft.py
+++ social/tests/backends/test_lyft.py
@@ -4,15 +4,25 @@ from social.tests.backends.oauth import OAuth2Test
 
 
 class LyftOAuth2Test(OAuth2Test):
+
     backend_path = 'social.backends.lyft.LyftOAuth2'
+
+    user_data_url = \
+        'https://api.lyft.com/v1/profile'
+
     access_token_body = json.dumps({
-        'access_token': 'foobar',
-        'refresh_token': 'barfoo',
+        'access_token': 'atoken_foo',
+        'refresh_token': 'rtoken_bar',
         'token_type': 'bearer',
         'expires_in': 3600,
-        'scope': 'one two three'
+        'scope': 'public profile rides.read rides.request',
+        'id': 'user_foobar'
     })
-    # expected_username = 'acct_foobar'
+
+    user_data_body = json.dumps({
+        'id': 'user_foobar'
+    })
+    expected_username = 'user_foobar'
 
     def test_login(self):
         self.do_login()
