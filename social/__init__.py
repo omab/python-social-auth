d7f556edb8ae2f129a4ac6cde31184c3c211acef
Matías Aguirre <matiasaguirre@gmail.com>
2017-01-26 13:14:14 -0300

    Version bump 0.3.4

--- social/__init__.py
+++ social/__init__.py
@@ -2,6 +2,6 @@
 python-social-auth application, allows OpenId or OAuth user
 registration/authentication just adding a few configurations.
 """
-version = (0, 3, 3)
+version = (0, 3, 4)
 extra = ''
 __version__ = '.'.join(map(str, version)) + extra
