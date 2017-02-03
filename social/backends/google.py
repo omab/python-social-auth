f923691790086e6bd3ab79b93f39680e97286d09
Matías Aguirre <matiasaguirre@gmail.com>
2017-02-03 09:19:27 -0300

    Fix import line

--- social/backends/google.py
+++ social/backends/google.py
@@ -1,3 +1,3 @@
 from social_core.backends.google import BaseGoogleAuth, BaseGoogleOAuth2API, \
     GoogleOAuth2, GooglePlusAuth, GoogleOAuth, GoogleOpenId
-from social_core.backends.google_openidconnect GoogleOpenIdConnect
+from social_core.backends.google_openidconnect import GoogleOpenIdConnect
